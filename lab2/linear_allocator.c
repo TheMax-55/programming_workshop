20f9a2d9271b7e8be352a9041819b3a2b98e92f6
orlovf <orlovf143@gmail.com>
2025-06-03 07:52:29 +0000

    feat: add linear allocator

--- lab2/linear_allocator.c
+++ lab2/linear_allocator.c
@@ -1,1 +1,63 @@
-// linear_allocator.c
+#include "linear_allocator.h"
+
+linear_allocator *linear_allocator_init(size_t size) {
+  if (size == 0)
+    return NULL;
+
+  linear_allocator *allocator =
+      (linear_allocator *)malloc(sizeof(linear_allocator));
+  if (allocator == NULL) {
+    return NULL;
+  }
+
+  allocator->memory_block = malloc(size);
+  if (allocator->memory_block == NULL) {
+    free(allocator);
+    return NULL;
+  }
+
+  allocator->total_size = size;
+  allocator->offset = 0;
+  return allocator;
+}
+
+void linear_allocator_deinit(linear_allocator *allocator) {
+  if (allocator == NULL) {
+    return;
+  }
+
+  if (allocator->memory_block != NULL) {
+    free(allocator->memory_block);
+  }
+  free(allocator);
+}
+
+void *linear_allocator_alloc(linear_allocator *allocator, size_t size) {
+  if (!allocator || size == 0) {
+    return NULL;
+  }
+
+  if (allocator->offset > allocator->total_size ||
+      size > allocator->total_size - allocator->offset) {
+    return NULL;
+  }
+
+  void *address = (char *)allocator->memory_block + allocator->offset;
+  allocator->offset += size;
+  return address;
+}
+
+void linear_allocator_reset(linear_allocator *allocator) {
+  if (allocator == NULL) {
+    return;
+  }
+  allocator->offset = 0;
+}
+
+void *linear_alloc(void *allocator, size_t size) {
+  return linear_allocator_alloc((linear_allocator *)allocator, size);
+}
+
+void linear_reset(void *allocator) {
+  linear_allocator_reset((linear_allocator *)allocator);
+}
--- lab2/linear_allocator_test.c
+++ lab2/linear_allocator_test.c
@@ -1,1 +1,83 @@
-int main() {}
+#include "linear_allocator.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void linear_allocator_init_test() {
+
+  size_t size = 16 * 1024;
+  linear_allocator *alloc = linear_allocator_init(size);
+
+  assert(alloc != NULL);
+  assert(alloc->memory_block != NULL);
+  assert(alloc->total_size == size);
+  assert(alloc->offset == 0);
+
+  linear_allocator_deinit(alloc);
+}
+
+void linear_allocator_reset_test() {
+
+  size_t size = 32 * 1024;
+  linear_allocator *alloc = linear_allocator_init(size);
+
+  void *ptr1 = linear_allocator_alloc(alloc, sizeof(void *));
+  linear_allocator_reset(alloc);
+  void *ptr2 = linear_allocator_alloc(alloc, sizeof(void *));
+
+  assert(ptr1 == ptr2);
+  linear_allocator_deinit(alloc);
+}
+
+void linear_allocator_alloc_test() {
+
+  linear_allocator *alloc = linear_allocator_init(16 * 1024);
+
+  int *ptr1 = linear_allocator_alloc(alloc, sizeof(int));
+  assert(ptr1 != NULL);
+
+  *ptr1 = 2;
+  assert(*ptr1 == 2);
+  assert(alloc->offset == sizeof(int));
+
+  int *ptr2 = linear_allocator_alloc(alloc, sizeof(int));
+  assert(ptr2 != NULL);
+
+  *ptr2 = 10;
+  assert(*ptr2 == 10);
+  assert(alloc->offset == sizeof(int) + sizeof(int));
+
+  linear_allocator_deinit(alloc);
+}
+
+void linear_allocator_out_of_memory_test() {
+
+  size_t size = 2 * 1024;
+  linear_allocator *alloc = linear_allocator_init(size);
+
+  int *ptr = linear_allocator_alloc(alloc, 10 * 1024);
+
+  assert(ptr == NULL);
+  linear_allocator_deinit(alloc);
+}
+
+void linear_allocator_zero_byte_test() {
+
+  size_t size = 16 * 1024;
+  linear_allocator *alloc = linear_allocator_init(size);
+
+  int *ptr = linear_allocator_alloc(alloc, 0);
+
+  assert(ptr == NULL);
+
+  linear_allocator_deinit(alloc);
+}
+
+int main() {
+  linear_allocator_init_test();
+  linear_allocator_alloc_test();
+  linear_allocator_out_of_memory_test();
+  linear_allocator_reset_test();
+  linear_allocator_zero_byte_test();
+  return 0;
+}
